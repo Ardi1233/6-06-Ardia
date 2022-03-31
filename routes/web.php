29b467b501697f3c9d38ea4fcc71d75590b278a0
Ardi <3103120029@student.smktelkom-pwt.sch.id>
2022-03-31 19:58:08 +0700

    modul 7

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContractController;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
+|~
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/', function () {
-    return view('home', [
+    return view('index', [
         "title" => "Home"
     ]);
 });
 
 Route::get('/about', function () {
-    return view('about' , [
-     "title" => "About",
-     "nama" => "Ardia Alif Ramadhan",
-     "email" => "3103120029@gmail.com",
-     "gambar" => "FotoArdi.jpg"
+    return view('about',[
+    "title" => "About",    
+    "nama" => "Ardia Alif Ramadhan",
+    "email" => "3103120029@student.smktelkom-pwt.sch.id",
+    "gambar" => "FotoArdi.jpg"
     ]);
 });
 
@@ -38,4 +36,20 @@ Route::get('/gallery', function () {
         "title" => "Gallery"
     ]);
 });
-Route::resource('/contacts', ContractController::class);
+
+
+
+//Route::resource('/contacts', ContactController::class);
+Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+Route::post('/contacts/store', [ContactController::class, 'store'])->name('contacts.store');
+
+Auth::routes();
+
+
+Route::group(['middleware' => ['auth']], function(){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/contacts/index', [ContactController::class, 'index'])->name('contacts.index');
+    Route::get('/contacts/{id}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+    Route::post('/contacts/{id}/update', [ContactController::class, 'update'])->name('contacts.update');
+    Route::get('/contacts/{id}/destroy', [ContactController::class, 'destroy'])->name('contacts.destroy');
+});
